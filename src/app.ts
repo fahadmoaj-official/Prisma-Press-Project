2790a3e57e88380660a2ffdce8bd461624f2c200
Fahad Sarker <moajfahad@gmail.com>
2026-07-02 16:39:24 +0600

    create register user On app file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import env from "./config/env";
 import cors from "cors";
+import httpStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 const app = express();
 
 
@@ -19,6 +22,82 @@ app.use(cookieParser());
 app.get("/", (req:Request, res:Response) => {
     res.send("Hello, World!");
 });
+
+
+app.post("/api/auth/register", async (req:Request, res:Response) => {
+
+try {
+
+    const {name, email, password,profilePhoto,bio,role} = req.body;
+
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            email: email,
+        }
+    });
+
+    if(isUserExist) {
+        return res.status(httpStatus.CONFLICT).json({
+            message: "User already exists",
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password,Number(env.bcryptSaltRounds));
+
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: hashedPassword,
+            role: role || "USER",
+        }
+    });
+
+
+    await prisma.profile.create({
+        data: {
+            userId: user.id,
+            profilePhoto: profilePhoto,
+            bio: bio
+        }
+    });
+
+    const profile = await prisma.user.findUnique({
+        where: {
+            id: user.id,
+            email: user.email || email,
+        },
+        omit:{
+            password: true,
+        },
+        include: {
+            profile: true,
+        }
+    });
+
+
+
+    res.status(httpStatus.CREATED).json({
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "Data received successfully",
+        data: {
+            user: profile
+        }
+    });
+
+    }catch (error) {
+        console.error(error);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+            message: "Internal server error while registering user",
+            error: error instanceof Error ? error.message : "Unknown error",
+        });
+    }
+
+    
+})
 
 
 
--- src/config/env.ts
+++ src/config/env.ts
@@ -8,6 +8,7 @@ const env = {
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
   NODE_ENV: process.env.NODE_ENV || "development",
   APP_URL: process.env.APP_URL,
+  bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
 };
 
 export default env;
